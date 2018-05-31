993cda8679757dd8b8c24875fd4967af592754d9
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-05-31 14:56:54 +0200

    Fix reducer

--- comments.js
+++ comments.js
@@ -1,7 +1,5 @@
 import { ADD_COMMENT, EDIT_COMMENT, DELTE_COMMENT, THUMB_DOWN_COMMENT, THUMB_UP_COMMENT } from './actions';
 function comments(state = [], action) {
-    const others = state.filter(comment => comment.id !== action.id);
-    const commentToUpdate = state.find(comment => comment.id === action.id);
     switch (action.type) {
         case ADD_COMMENT:
             return [{
@@ -10,14 +8,21 @@ function comments(state = [], action) {
                 votes: 0
             }, ...state];
         case DELTE_COMMENT:
+            const others = state.filter(comment => comment.id !== action.id);
             return others;
         case EDIT_COMMENT:
             commentToUpdate.text = action.text;
+            const others = state.filter(comment => comment.id !== action.id);
+            const commentToUpdate = state.find(comment => comment.id === action.id);
             return [commentToUpdate, ...others];
         case THUMB_UP_COMMENT:
+            const others = state.filter(comment => comment.id !== action.id);
+            const commentToUpdate = state.find(comment => comment.id === action.id);
             commentToUpdate.votes++;
             return [commentToUpdate, ...others];
         case THUMB_DOWN_COMMENT:
+            const others = state.filter(comment => comment.id !== action.id);
+            const commentToUpdate = state.find(comment => comment.id === action.id);
             commentToUpdate.votes--;
             return [commentToUpdate, ...others];
         default:

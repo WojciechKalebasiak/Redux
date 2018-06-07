f2c7a7adde7f80cef97526475212996a1effb50a
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-06-07 17:51:44 +0200

    Fix comments.js

--- comments.js
+++ comments.js
@@ -11,20 +11,26 @@ function comments(state = [], action) {
             const others = state.filter(comment => comment.id !== action.id);
             return others;
         case EDIT_COMMENT:
-            commentToUpdate.text = action.text;
-            const others = state.filter(comment => comment.id !== action.id);
-            const commentToUpdate = state.find(comment => comment.id === action.id);
-            return [commentToUpdate, ...others];
+            const updatedComments = state.map(comment => {
+                if (comment.id === action.id)
+                    comment.text = action.text;
+                return comment;
+            });
+            return updatedComments;
         case THUMB_UP_COMMENT:
-            const others = state.filter(comment => comment.id !== action.id);
-            const commentToUpdate = state.find(comment => comment.id === action.id);
-            commentToUpdate.votes++;
-            return [commentToUpdate, ...others];
+            const updatedComments = state.map(comment => {
+                if(comment.id===action.id)
+                    comment.votes++;
+                return comment;
+            });
+            return updatedComments;
         case THUMB_DOWN_COMMENT:
-            const others = state.filter(comment => comment.id !== action.id);
-            const commentToUpdate = state.find(comment => comment.id === action.id);
-            commentToUpdate.votes--;
-            return [commentToUpdate, ...others];
+            const updatedComments = state.map(comment => {
+                if (comment.id === action.id)
+                    comment.votes--;
+                return comment;
+            });
+            return updatedComments;
         default:
             return state;
     }
